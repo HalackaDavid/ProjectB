3da76a69453657d71b6367d5652c9a3fc04027e8
name@whatever <email@whatever>
2025-10-09 05:43:32 +0000

    aa

--- index.php
+++ index.php
@@ -8,12 +8,211 @@
     <div class="container">
         <h2>Nadpis</h1>
         <div class="list">
-            <?php
-                for($i = 0; $i < 30; $i++) {
-                    echo "<div class='item'><div>title</div><div class='desc'>desc</div></div>";
-                }
-            ?>
+            
+            <a href="#">
+                <div class="item">
+                    <div class="title">Hirošima a Nagasaki</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Operace Anthropoid</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Vylodění v Normandii</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Nicholas Winton</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Osvětim</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Emil Hácha</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Útok na Pearl Harbor</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Operace Barbarosa</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Život dětí v Terezíně</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Benito Mussolini</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Heydrichiáda</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Koncentrační tábor Revensbrück</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Winston Churchil</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Irena Sendlerová</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Hirohito</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Josef Galčík / Jan Kubiš</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Operace v Severní Africe</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Lidice - Obec která zmizela</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Zbrojení celého světa</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Vznik OSN</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Neville Chamberlain</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Vylodění v Italii</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Josef Stalin</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Varšavské Povstání</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Oskar Schindler</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Holokaust</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Rudolf Vrba a útěk z Osvětimy</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Příběhy Václava Morávka</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="item">
+                    <div class="title">Anna Franková</div>
+                    <div class="desc">desc</div>
+                </div>
+            </a>
+
         </div>
     </div>
 </body>
-</html
+</html>
